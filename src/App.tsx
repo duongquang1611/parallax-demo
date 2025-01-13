75b033389625d93c9e232fb77c18d3f0e37a3809
Dương Quang <duongquang16111997@gmail.com>
2025-01-13 22:26:29 +0700

    feat: solution 2 scroll bg2

--- src/App.tsx
+++ src/App.tsx
@@ -6,50 +6,42 @@ const App = () => {
   const bgRef = useRef<HTMLDivElement | null>(null);
 
   // SOLUTION 1: scroll bg
-  const handleScroll = () => {
-    const bg = document.querySelector(".bg") as HTMLElement;
-    const container = document.querySelector(".container") as HTMLElement;
-
-    if (bg && container) {
-      const containerHeight = container.scrollHeight - window.innerHeight; // Phần cuộn thực tế
-      const bgHeight = bg.offsetHeight; // Chiều cao thực tế của ảnh
-      const scrollY = window.scrollY; // Vị trí cuộn hiện tại
-      // Tính tỉ lệ di chuyển dựa trên sự chênh lệch chiều cao
-      const scrollRatio = scrollY / containerHeight;
-
-      // Dịch chuyển ảnh theo tỉ lệ
-      const translateY = (container.scrollHeight - bgHeight) * scrollRatio;
-
-      bg.style.transform = `translateY(${translateY}px)`;
-    }
-  };
-
-  // SOLUTION 2: scroll bg2
   // const handleScroll = () => {
-  //   // const bg = document.querySelector(".bg") as HTMLElement;
-  //   const bg = document.querySelector(".bg2") as HTMLElement;
+  //   const bg = document.querySelector(".bg") as HTMLElement;
   //   const container = document.querySelector(".container") as HTMLElement;
 
   //   if (bg && container) {
   //     const containerHeight = container.scrollHeight - window.innerHeight; // Phần cuộn thực tế
   //     const bgHeight = bg.offsetHeight; // Chiều cao thực tế của ảnh
   //     const scrollY = window.scrollY; // Vị trí cuộn hiện tại
-
-  //     // Tính tỉ lệ di chuyển
+  //     // Tính tỉ lệ di chuyển dựa trên sự chênh lệch chiều cao
   //     const scrollRatio = scrollY / containerHeight;
 
   //     // Dịch chuyển ảnh theo tỉ lệ
-  //     // const translateY = (container.scrollHeight - bgHeight) * scrollRatio;
-  //     // console.log("---App---", (bgHeight / 2) * scrollRatio);
-  //     bg.scrollTo({
-  //       top: (bgHeight / 2) * scrollRatio,
-  //       behavior: "smooth",
-  //     });
-  //     // Áp dụng transform
-  //     // bg.style.transform = `translateY(${translateY}px)`;
-  //     // bg.style.transform = `translate3d(0, ${translateY}px, 0)`;
+  //     const translateY = (container.scrollHeight - bgHeight) * scrollRatio;
+
+  //     bg.style.transform = `translateY(${translateY}px)`;
   //   }
   // };
+
+  // SOLUTION 2: scroll bg2
+  const handleScroll = () => {
+    const bg = bgRef.current;
+    const container = containerRef.current;
+
+    if (bg && container) {
+      const containerHeight = container.scrollHeight - window.innerHeight; // Phần cuộn thực tế
+      const bgHeight = bg.offsetHeight;
+      const scrollY = window.scrollY;
+
+      const scrollRatio = scrollY / containerHeight;
+
+      bg.scrollTo({
+        // because img height 150%
+        top: (bgHeight / 2) * scrollRatio,
+      });
+    }
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -60,10 +52,10 @@ const App = () => {
 
   return (
     <div className="container" ref={containerRef}>
-      <div className="bg"></div>
-      {/* <div className="bg2" ref={bgRef}>
+      {/* <div className="bg"></div> */}
+      <div className="bg2" ref={bgRef}>
         <div className="img2"></div>
-      </div> */}
+      </div>
       {/* <img className="bg" src={"src/assets/sir.jpg"} /> */}
       {/* <div className="content">QQQQQQ</div> */}
       {/* <img src="rock1.png" className="rock1" alt="rock1" />
